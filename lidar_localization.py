e76d20796c27c7cdae359195ffba1e89179c168a
Dorian Konrad <dorian.konrad@gmail.com>
2016-03-16 22:11:27 +0100

    Mostly cosmetic (viewer) changes

--- lidar_localization.py
+++ lidar_localization.py
@@ -1,20 +1,25 @@
 
 import math, time, itertools
 import numpy as np
-from pyqtgraph.Qt import QtGui, QtCore
-import pyqtgraph as pg
+import os
+
 import zmqmsgbus
 import convex_area_localization
 from convex_area_localization import *
 
-PLOT = False # activate plot - this reduce drastically the computation rate
-PRINT = False # print position
+PLOT = True # activate plot - this reduce drastically the computation rate
+PRINT = True # print position
+
+if PLOT:
+    from pyqtgraph.Qt import QtGui, QtCore
+    import pyqtgraph as pg
+
 
 TIM561_START_ANGLE = -0.7853981634  # in rad,  = -45°
 TIM561_STOP_ANGLE  =  3.926990817   # in rad,  = 225°
 
 MAX_DIST_POINT = 0.005  # in cm
-NB_LINE = 4            # number of line to search in points cloud
+NB_LINE = 5             # number of line to search in points cloud
 
 def PlotPolar():
     # Add polar grid lines
@@ -92,15 +97,15 @@ while 1:
 
     # keep external segment
     ext_segments = keep_external_segment(segments)
-
+    
     # extract valide intersection
     corners = extract_corner(ext_segments)
 
     # localize the robot using the intersection found
     pos, orientation = localize(corners, position)
-    if pos is not None or orientation is not None:
+    if pos is not None and orientation is not None:
         if PRINT:
-            print(chr(27) + "[2J")
+            os.system('clear')
             print(str(np.mean(pos, axis=0))+" "+str(np.mean(orientation)))
 
         node.publish('/lidar/position', np.mean(pos, axis=0).tolist())
@@ -108,7 +113,9 @@ while 1:
 
 
     # draw raw measure
-    # ----------------
+    # ----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
+    
     if PLOT:
         plot.clear()
         PlotPolar()
@@ -117,25 +124,41 @@ while 1:
         plot.plot(cloud_pts[:,0],cloud_pts[:,1],pen=None,symbol='o',
                   symbolPen=None,symbolSize=7,symbolBrush=(255,255,255,50))
 
+
+        plot.plot(red_cloud_pts[:,0],red_cloud_pts[:,1],pen=None,symbol='o',
+                  symbolPen=None,symbolSize=7,symbolBrush=(125,125,0,50))
+
+
+        convexHulls = np.asarray(convex_hull(cloud_pts.tolist()))
+        plot.plot(convexHulls[:,0],convexHulls[:,1],pen=None,symbol='o',
+                  symbolPen=None,symbolSize=7,symbolBrush=(255,0,0,255))
+
         linePen = pg.mkPen(color=(200,200,200,200),width=2,
             style=QtCore.Qt.DotLine)
         colors=[(255,0,0,100),(0,255,0,100),(0,0,255,100),(255,255,0,100),
                 (255,0,255,100),(0,127,255,100),(127,0,0,100),(0,127,0,100),
                 (0,0,127,100),(127,255,0,100),(127,0,255,100),(0,127,255,100)]
 
+
+        colors2=[(255,0,0,255),(0,255,0,255),(0,0,255,255),(255,255,0,255),
+                (255,0,255,255),(0,127,255,255),(127,0,0,255),(0,127,0,255),
+                (0,0,127,255),(127,255,0,255),(127,0,255,255),(0,127,255,255)]
+
         # draw segment
-        for idx, segment in enumerate(segments):
-            linePen = pg.mkPen(color=colors[idx],width=2,
-                style=QtCore.Qt.SolidLine)
-            plot.plot((segment.pt_A[0],segment.pt_B[0]),
-                        (segment.pt_A[1],segment.pt_B[1]), pen=linePen)
-        
-        # draw external segment
         for idx, segment in enumerate(ext_segments):
-            linePen = pg.mkPen(color=(255,0,0,255),width=4,
+            linePen = pg.mkPen(color=colors2[idx],width=4,
                 style=QtCore.Qt.SolidLine)
             plot.plot((segment.pt_A[0],segment.pt_B[0]),
                         (segment.pt_A[1],segment.pt_B[1]), pen=linePen)
+
+        # draw table estimation corner
+        table_corner = np.array([[0,0],[0,3],[2,3],[2,0]], dtype=float)
+        table_corner = table_corner - [position[0], position[1]]
+        table_corner = rotatePolygon(table_corner, -position[2]+math.pi/2)
+
+        symbolePen = pg.mkPen(color=(255,255,255,255), width= 2)
+        plot.plot(table_corner[:,0], table_corner[:,1], 
+                pen=None,  symbol='x', symbolPen=symbolePen, symbolSize=12)
 
         # draw corners found
         symbolePen = pg.mkPen(color=(0,0,255,255), width= 2)
@@ -144,14 +167,9 @@ while 1:
             corner_y = [corner.y for corner in corners]
             plot.plot(corner_x, corner_y, pen=None, symbol='x',
                 symbolPen=symbolePen)
-        
-        # draw table estimation corner
-        table_corner = np.array([[0,0],[0,3],[2,3],[2,0]], dtype=float)
-        table_corner = table_corner - [position[0], position[1]]
-        table_corner = rotatePolygon(table_corner, -position[2])
 
-        symbolePen = pg.mkPen(color=(255,255,255,255), width= 2)
-        plot.plot(table_corner[:,0], table_corner[:,1], 
-                pen=None,  symbol='x', symbolPen=symbolePen, symbolSize=12)
+        
 
         app.processEvents()
+
+
