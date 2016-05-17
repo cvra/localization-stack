4af4b49a6d4882f7f98e597df8b44e6474f8a51a
Dorian Konrad <dorian.konrad@gmail.com>
2016-05-17 17:50:00 +0200

    Rewrite module to accept arguments and config file

--- lidar_localization.py
+++ lidar_localization.py
@@ -1,5 +1,10 @@
 
-import math, time, itertools
+import argparse
+import json
+from jsmin import jsmin
+import math 
+import time
+import itertools
 import numpy as np
 import os
 
@@ -7,21 +12,23 @@ import zmqmsgbus
 import convex_area_localization
 from convex_area_localization import *
 
-PLOT = True # activate plot - this reduce drastically the computation rate
-PRINT = True # print position
-
-if PLOT:
-    from pyqtgraph.Qt import QtGui, QtCore
-    import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
 
 
-TIM561_START_ANGLE = -0.7853981634  # in rad,  = -45°
-TIM561_STOP_ANGLE  =  3.926990817   # in rad,  = 225°
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
 
-MAX_DIST_POINT = 0.005  # in cm
-NB_LINE = 5             # number of line to search in points cloud
+    parser.add_argument('config', type=argparse.FileType('r'), help='File where the configuration is stored.')
 
-def PlotPolar():
+    parser.add_argument('--plot', help='Plot LIDAR points. Activate this option reduce drastically the computation rate !', action="store_true")
+
+    parser.add_argument('--print', dest='print_output', help='Print estimated postion and orientation in the shell.', action="store_true")
+
+    return parser.parse_args()
+
+
+def PlotPolar(plot):
     # Add polar grid lines
     plot.addLine(x=0, pen=0.2)
     plot.addLine(y=0, pen=0.2)
@@ -40,6 +47,7 @@ def PlotPolar():
         plot.setXRange(-3, 3)
         plot.setYRange(-3, 3)
 
+
 def pol2cart(radius, theta):
     ''' Convert polar coordinate onto cartesian coordinate
 
@@ -52,124 +60,138 @@ def pol2cart(radius, theta):
     cartesian = np.array([x, y]).T;
     return cartesian;
 
+
 def update_scan_data(topic, message):
     global datagram
     datagram = message
+
 
 def update_scan_pos(topic, message):
     global datagram_pos
     datagram_pos = message
 
-# PyQtGraph stuff
-if PLOT:
-    app = QtGui.QApplication([])
-    pg.setConfigOptions(antialias=True)
-    plot = pg.plot(title='Lidar Polar Plot')
-    plot.resize(600,600)
-    plot.setAspectLocked()
-
-# zmqmsgbus subscribe
-bus = zmqmsgbus.Bus(sub_addr='ipc://ipc/source',
-                    pub_addr='ipc://ipc/sink')
-node = zmqmsgbus.Node(bus)
-
-datagram = node.recv('/lidar/scan')
-node.register_message_handler('/lidar/scan', update_scan_data)
-datagram_pos = node.recv('/odometry/position')
-node.register_message_handler('/odometry/position', update_scan_pos)
-
-print('receiving')
-while 1:
-    position = np.asarray(datagram_pos)
-    radius = np.asarray(datagram['Data'])
-    theta = np.linspace(TIM561_START_ANGLE, TIM561_STOP_ANGLE, len(radius))
-
-    cloud_pts = pol2cart(radius, theta)
-    red_cloud_pts = density_reduction(cloud_pts, MAX_DIST_POINT, 1)
-    red_cloud_pts = keep_border_points(red_cloud_pts)
-
-
-    # find lines
-    lines_model = find_lines(red_cloud_pts, NB_LINE)
-
-    # convert into segment
-    segments = segment_line(lines_model)
-
-    # keep external segment
-    ext_segments = keep_external_segment(segments)
     
-    # extract valide intersection
-    corners = extract_corner(ext_segments)
+def main():
+    global datagram
+    global datagram_pos
 
-    # localize the robot using the intersection found
-    pos, orientation = localize(corners, position)
-    if pos is not None and orientation is not None:
-        if PRINT:
-            os.system('clear')
-            print(str(np.mean(pos, axis=0))+" "+str(np.mean(orientation)))
+    args = parse_args()
+    config = json.loads(jsmin(args.config.read()))
 
-        node.publish('/lidar/position', np.mean(pos, axis=0).tolist())
-        node.publish('/lidar/orientation', np.mean(orientation))
+    # PyQtGraph stuff
+    if args.plot:
+        app = QtGui.QApplication([])
+        pg.setConfigOptions(antialias=False)
+        plot = pg.plot(title='Lidar Polar Plot')
+        plot.resize(600,600)
+        plot.setAspectLocked()
 
+    # zmqmsgbus subscribe
+    bus = zmqmsgbus.Bus(sub_addr='ipc://ipc/source',
+                        pub_addr='ipc://ipc/sink')
+    node = zmqmsgbus.Node(bus)
 
-    # draw raw measure
-    # ----------------------------------------------------------------------
-    # ----------------------------------------------------------------------
-    
-    if PLOT:
-        plot.clear()
-        PlotPolar()
+    datagram = node.recv('/lidar/scan')
+    node.register_message_handler('/lidar/scan', update_scan_data)
+    datagram_pos = node.recv('/odometry/position')
+    node.register_message_handler('/odometry/position', update_scan_pos)
 
-        # draw lidar points
-        plot.plot(cloud_pts[:,0],cloud_pts[:,1],pen=None,symbol='o',
-                  symbolPen=None,symbolSize=7,symbolBrush=(255,255,255,50))
+    datagram_pos = np.asarray([0,1,0])
 
+    print('receiving')
+    while 1:
+        radius = np.asarray(datagram['Data'])
+        theta = np.linspace(config['TIM561_START_ANGLE'], config['TIM561_STOP_ANGLE'], len(radius))
+        cloud_pts = pol2cart(radius, theta)
 
-        plot.plot(red_cloud_pts[:,0],red_cloud_pts[:,1],pen=None,symbol='o',
-                  symbolPen=None,symbolSize=7,symbolBrush=(125,125,0,50))
-
-
-        convexHulls = np.asarray(convex_hull(cloud_pts.tolist()))
-        plot.plot(convexHulls[:,0],convexHulls[:,1],pen=None,symbol='o',
-                  symbolPen=None,symbolSize=7,symbolBrush=(255,0,0,255))
-
-        linePen = pg.mkPen(color=(200,200,200,200),width=2,
-            style=QtCore.Qt.DotLine)
-        colors=[(255,0,0,100),(0,255,0,100),(0,0,255,100),(255,255,0,100),
-                (255,0,255,100),(0,127,255,100),(127,0,0,100),(0,127,0,100),
-                (0,0,127,100),(127,255,0,100),(127,0,255,100),(0,127,255,100)]
-
-
-        colors2=[(255,0,0,255),(0,255,0,255),(0,0,255,255),(255,255,0,255),
-                (255,0,255,255),(0,127,255,255),(127,0,0,255),(0,127,0,255),
-                (0,0,127,255),(127,255,0,255),(127,0,255,255),(0,127,255,255)]
-
-        # draw segment
-        for idx, segment in enumerate(ext_segments):
-            linePen = pg.mkPen(color=colors2[idx],width=4,
-                style=QtCore.Qt.SolidLine)
-            plot.plot((segment.pt_A[0],segment.pt_B[0]),
-                        (segment.pt_A[1],segment.pt_B[1]), pen=linePen)
-
-        # draw table estimation corner
-        table_corner = np.array([[0,0],[0,3],[2,3],[2,0]], dtype=float)
-        table_corner = table_corner - [position[0], position[1]]
-        table_corner = rotatePolygon(table_corner, -position[2]+math.pi/2)
-
-        symbolePen = pg.mkPen(color=(255,255,255,255), width= 2)
-        plot.plot(table_corner[:,0], table_corner[:,1], 
-                pen=None,  symbol='x', symbolPen=symbolePen, symbolSize=12)
-
-        # draw corners found
-        symbolePen = pg.mkPen(color=(0,0,255,255), width= 2)
-        if corners is not None:
-            corner_x = [corner.x for corner in corners]
-            corner_y = [corner.y for corner in corners]
-            plot.plot(corner_x, corner_y, pen=None, symbol='x',
-                symbolPen=symbolePen)
-
+        # reduce cloud point density
+        red_cloud_pts = density_reduction(cloud_pts, config['MAX_DIST_POINT'], 1)
         
+        # keep only border points
+        red_cloud_pts = keep_border_points(red_cloud_pts)
 
-        app.processEvents()
+        # find lines
+        lines_model = find_lines(red_cloud_pts, config['NB_LINE'])
+
+        # convert lines onto segments
+        segments = segment_line(lines_model)
+
+        # only keep external segments
+        ext_segments = keep_external_segment(segments)
+        
+        # extract valide intersections
+        corners = extract_corner(ext_segments)
+
+        # localize the robot using the intersections found
+        positions, orientations = localize(corners, datagram_pos)
+
+        position = np.mean(positions, axis=0).squeeze()
+        orientation = np.mean(orientations)
+
+        if position is not None and orientation is not None:
+            if args.print_output:
+                os.system('clear')
+                print(str(position)+" "+str(orientation))
+
+            node.publish('/lidar/position', np.mean(position, axis=0).tolist())
+            node.publish('/lidar/orientation', np.mean(orientation))
 
 
+        # draw raw measure
+        # ----------------------------------------------------------------------
+        # ----------------------------------------------------------------------
+
+        if args.plot:
+            plot.clear()
+            PlotPolar(plot)
+
+            # draw lidar points
+            plot.plot(cloud_pts[:,0],cloud_pts[:,1],pen=None,symbol='o',
+                      symbolPen=None,symbolSize=7,symbolBrush=(255,255,255,50))
+
+
+            plot.plot(red_cloud_pts[:,0],red_cloud_pts[:,1],pen=None,symbol='o',
+                      symbolPen=None,symbolSize=7,symbolBrush=(125,125,0,50))
+
+
+            convexHulls = np.asarray(convex_hull(cloud_pts.tolist()))
+            plot.plot(convexHulls[:,0],convexHulls[:,1],pen=None,symbol='o',
+                      symbolPen=None,symbolSize=7,symbolBrush=(255,0,0,255))
+
+            linePen = pg.mkPen(color=(200,200,200,200),width=2,
+                style=QtCore.Qt.DotLine)
+
+            colors=[(255,0,0,255),(0,255,0,255),(0,0,255,255),(255,255,0,255),
+                    (255,0,255,255),(0,127,255,255),(127,0,0,255),(0,127,0,255),
+                    (0,0,127,255),(127,255,0,255),(127,0,255,255),(0,127,255,255)]
+
+            # draw segment
+            for idx, segment in enumerate(ext_segments):
+                linePen = pg.mkPen(color=colors[idx],width=4,
+                    style=QtCore.Qt.SolidLine)
+                plot.plot((segment.pt_A[0],segment.pt_B[0]),
+                            (segment.pt_A[1],segment.pt_B[1]), pen=linePen)
+
+            
+            # draw corners found
+            symbolePen = pg.mkPen(color=(0,0,255,255), width= 2)
+            if corners is not None:
+                corner_x = [corner.x for corner in corners]
+                corner_y = [corner.y for corner in corners]
+                plot.plot(corner_x, corner_y, pen=None, symbol='x',
+                    symbolPen=symbolePen)
+
+            # # draw table estimation corner
+            table_corner = np.array([[0,0],[0,3],[2,3],[2,0]], dtype=float)
+            table_corner = table_corner - [position[0], position[1]]
+            table_corner = rotatePolygon(table_corner, -orientation+math.pi/2)
+
+            symbolePen = pg.mkPen(color=(255,255,255,255), width= 2)
+            plot.plot(table_corner[:,0], table_corner[:,1], 
+                    pen=None,  symbol='x', symbolPen=symbolePen, symbolSize=12)
+
+
+            app.processEvents()
+
+if __name__ == '__main__':
+    main()
