d723f1847a6952f3c907b0463286b14e577a8cdd
Dorian Konrad <dorian.konrad@gmail.com>
2016-05-18 15:21:55 +0200

    Separate localization from viewer

--- lidar_localization.py
+++ lidar_localization.py
@@ -12,53 +12,14 @@ import zmqmsgbus
 import convex_area_localization
 from convex_area_localization import *
 
-from pyqtgraph.Qt import QtGui, QtCore
-import pyqtgraph as pg
-
-
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument('config', type=argparse.FileType('r'), help='File where the configuration is stored.')
 
-    parser.add_argument('--plot', help='Plot LIDAR points. Activate this option reduce drastically the computation rate !', action="store_true")
-
     parser.add_argument('--print', dest='print_output', help='Print estimated postion and orientation in the shell.', action="store_true")
 
     return parser.parse_args()
-
-
-def PlotPolar(plot):
-    # Add polar grid lines
-    plot.addLine(x=0, pen=0.2)
-    plot.addLine(y=0, pen=0.2)
-
-    radius = np.arange(0.2, 3, 0.2);
-    for r in radius:
-        circle = pg.QtGui.QGraphicsEllipseItem(-r, -r, r*2, r*2)
-        circle.setPen(pg.mkPen(color=(30, 30, 30)))
-        plot.addItem(circle)
-
-    radius = np.arange(1, 3.1, 1);
-    for r in radius:
-        circle = pg.QtGui.QGraphicsEllipseItem(-r, -r, r*2, r*2)
-        circle.setPen(pg.mkPen(color=(60, 60, 60)))
-        plot.addItem(circle)
-        plot.setXRange(-3, 3)
-        plot.setYRange(-3, 3)
-
-
-def pol2cart(radius, theta):
-    ''' Convert polar coordinate onto cartesian coordinate
-
-    Parameters are (N) numpy array of float
-    Return cartesian (N,2) numpy array of float '''
-
-    x = radius * np.cos(theta)
-    y = radius * np.sin(theta)
-
-    cartesian = np.array([x, y]).T;
-    return cartesian;
 
 
 def update_scan_data(topic, message):
@@ -77,14 +38,6 @@ def main():
 
     args = parse_args()
     config = json.loads(jsmin(args.config.read()))
-
-    # PyQtGraph stuff
-    if args.plot:
-        app = QtGui.QApplication([])
-        pg.setConfigOptions(antialias=False)
-        plot = pg.plot(title='Lidar Polar Plot')
-        plot.resize(600,600)
-        plot.setAspectLocked()
 
     # zmqmsgbus subscribe
     bus = zmqmsgbus.Bus(sub_addr='ipc://ipc/source',
@@ -135,64 +88,6 @@ def main():
 
             node.publish('/lidar/position', np.mean(position, axis=0).tolist())
             node.publish('/lidar/orientation', np.mean(orientation))
-
-
-        # draw raw measure
-        # ----------------------------------------------------------------------
-        # ----------------------------------------------------------------------
-
-        if args.plot:
-            plot.clear()
-            PlotPolar(plot)
-
-            # draw lidar points
-            plot.plot(cloud_pts[:,0],cloud_pts[:,1],pen=None,symbol='o',
-                      symbolPen=None,symbolSize=7,symbolBrush=(255,255,255,50))
-
-
-            plot.plot(red_cloud_pts[:,0],red_cloud_pts[:,1],pen=None,symbol='o',
-                      symbolPen=None,symbolSize=7,symbolBrush=(125,125,0,50))
-
-
-            convexHulls = np.asarray(convex_hull(cloud_pts.tolist()))
-            plot.plot(convexHulls[:,0],convexHulls[:,1],pen=None,symbol='o',
-                      symbolPen=None,symbolSize=7,symbolBrush=(255,0,0,255))
-
-            linePen = pg.mkPen(color=(200,200,200,200),width=2,
-                style=QtCore.Qt.DotLine)
-
-            colors=[(255,0,0,255),(0,255,0,255),(0,0,255,255),(255,255,0,255),
-                    (255,0,255,255),(0,127,255,255),(127,0,0,255),(0,127,0,255),
-                    (0,0,127,255),(127,255,0,255),(127,0,255,255),(0,127,255,255)]
-
-            # draw segment
-            for idx, segment in enumerate(ext_segments):
-                linePen = pg.mkPen(color=colors[idx],width=4,
-                    style=QtCore.Qt.SolidLine)
-                plot.plot((segment.pt_A[0],segment.pt_B[0]),
-                            (segment.pt_A[1],segment.pt_B[1]), pen=linePen)
-
-            
-            # draw corners found
-            symbolePen = pg.mkPen(color=(0,0,255,255), width= 2)
-            if corners is not None:
-                corner_x = [corner.x for corner in corners]
-                corner_y = [corner.y for corner in corners]
-                plot.plot(corner_x, corner_y, pen=None, symbol='x',
-                    symbolPen=symbolePen)
-
-            # # draw table estimation corner
-            table_corner = np.array([[0,0],[0,config['TABLE_HEIGHT']],[config['TABLE_WIDTH'],
-                                            config['TABLE_HEIGHT']],[config['TABLE_WIDTH'],0]], dtype=float)
-            table_corner = table_corner - [position[0], position[1]]
-            table_corner = rotatePolygon(table_corner, -orientation+math.pi/2)
-
-            symbolePen = pg.mkPen(color=(255,255,255,255), width= 2)
-            plot.plot(table_corner[:,0], table_corner[:,1], 
-                    pen=None,  symbol='x', symbolPen=symbolePen, symbolSize=12)
-
-
-            app.processEvents()
 
 if __name__ == '__main__':
     main()
